4e9975274cb8289d4aad8e9364037c09f8a374dd
ashishdawale20 <43884208+ashishdawale20@users.noreply.github.com>
2020-04-28 12:29:32 +0000

    Deploying to gh-pages from  @ 826aff739f74e293912affc89f8c01e327025204 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ced1f2f68325ac4e71cf12fe31e2f5a2.js"
+  "/precache-manifest.9e079e2e8e27de183059457b478e2c02.js"
 );
 
 self.addEventListener('message', (event) => {
